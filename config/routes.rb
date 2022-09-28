bda19143673f3671a1e4f346547ac82eeb3d57ae
前田紘基 <maedahiroki@maedakoukinoMacBook-Pro.local>
2022-09-29 03:18:49 +0900

    利用規約の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'overview', to: 'home#overview'
   get 'privacypolicy', to: 'home#privacypolicy'
+  get 'terms', to: 'home#terms'
 
   resources :songs, only: %i[index new create] do
     collection {get 'search'}
