8ad994437b3ee3cc7b26fe851d89622d24d90b57
前田紘基 <maedahiroki@maedakoukinoMacBook-Pro.local>
2023-03-03 01:05:52 +0900

    コメントのajax化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to post_path(comment.post), success: "コメントを作成しました！"
-    else
-      redirect_to post_path(comment.post), danger: "コメントの作成に失敗しました！"
-    end
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
   end
 
   def destroy
-    comment = current_user.comments.find(params[:id])
-    comment.destroy!
-    redirect_to post_path(comment.post), success: "投稿を削除しました！"
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
   end
 
   private
